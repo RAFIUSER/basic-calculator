ad1bc4c5e27f879007873b3823479d2b977b0916
Kgs. M. Rafiussyakban <rafiussyakban@gmail.com>
2026-09-17 20:18:04 +0700

    Add project files.

---
+++ Kalkulator Sederhana/Form1.Designer.vb
@@ -0,0 +1,288 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
+        Button5 = New Button()
+        Button6 = New Button()
+        Button7 = New Button()
+        Button8 = New Button()
+        Button9 = New Button()
+        Button10 = New Button()
+        Button11 = New Button()
+        Button12 = New Button()
+        Button13 = New Button()
+        Button14 = New Button()
+        Button15 = New Button()
+        Button16 = New Button()
+        Button17 = New Button()
+        Button18 = New Button()
+        Label1 = New Label()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Font = New Font("Microsoft Sans Serif", 18F, FontStyle.Bold)
+        Button1.Location = New Point(35, 332)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(136, 60)
+        Button1.TabIndex = 0
+        Button1.Text = "0"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Font = New Font("Microsoft Sans Serif", 18F, FontStyle.Bold)
+        Button2.Location = New Point(177, 332)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(65, 60)
+        Button2.TabIndex = 0
+        Button2.Text = "."
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Font = New Font("Microsoft Sans Serif", 18F, FontStyle.Bold)
+        Button3.Location = New Point(35, 266)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(65, 60)
+        Button3.TabIndex = 0
+        Button3.Text = "1"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Button4
+        ' 
+        Button4.Font = New Font("Microsoft Sans Serif", 18F, FontStyle.Bold)
+        Button4.Location = New Point(106, 266)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(65, 60)
+        Button4.TabIndex = 0
+        Button4.Text = "2"
+        Button4.UseVisualStyleBackColor = True
+        ' 
+        ' Button5
+        ' 
+        Button5.Font = New Font("Microsoft Sans Serif", 18F, FontStyle.Bold)
+        Button5.Location = New Point(35, 200)
+        Button5.Name = "Button5"
+        Button5.Size = New Size(65, 60)
+        Button5.TabIndex = 0
+        Button5.Text = "4"
+        Button5.UseVisualStyleBackColor = True
+        ' 
+        ' Button6
+        ' 
+        Button6.Font = New Font("Microsoft Sans Serif", 18F, FontStyle.Bold)
+        Button6.Location = New Point(106, 200)
+        Button6.Name = "Button6"
+        Button6.Size = New Size(65, 60)
+        Button6.TabIndex = 0
+        Button6.Text = "5"
+        Button6.UseVisualStyleBackColor = True
+        ' 
+        ' Button7
+        ' 
+        Button7.Font = New Font("Microsoft Sans Serif", 18F, FontStyle.Bold)
+        Button7.Location = New Point(35, 134)
+        Button7.Name = "Button7"
+        Button7.Size = New Size(65, 60)
+        Button7.TabIndex = 0
+        Button7.Text = "7"
+        Button7.UseVisualStyleBackColor = True
+        ' 
+        ' Button8
+        ' 
+        Button8.Font = New Font("Microsoft Sans Serif", 18F, FontStyle.Bold)
+        Button8.Location = New Point(106, 134)
+        Button8.Name = "Button8"
+        Button8.Size = New Size(65, 60)
+        Button8.TabIndex = 0
+        Button8.Text = "8"
+        Button8.UseVisualStyleBackColor = True
+        ' 
+        ' Button9
+        ' 
+        Button9.Font = New Font("Microsoft Sans Serif", 18F, FontStyle.Bold)
+        Button9.Location = New Point(177, 266)
+        Button9.Name = "Button9"
+        Button9.Size = New Size(65, 60)
+        Button9.TabIndex = 0
+        Button9.Text = "3"
+        Button9.UseVisualStyleBackColor = True
+        ' 
+        ' Button10
+        ' 
+        Button10.Font = New Font("Microsoft Sans Serif", 18F, FontStyle.Bold)
+        Button10.Location = New Point(248, 332)
+        Button10.Name = "Button10"
+        Button10.Size = New Size(65, 60)
+        Button10.TabIndex = 0
+        Button10.Text = "+"
+        Button10.UseVisualStyleBackColor = True
+        ' 
+        ' Button11
+        ' 
+        Button11.Font = New Font("Microsoft Sans Serif", 18F, FontStyle.Bold)
+        Button11.Location = New Point(177, 200)
+        Button11.Name = "Button11"
+        Button11.Size = New Size(65, 60)
+        Button11.TabIndex = 0
+        Button11.Text = "6"
+        Button11.UseVisualStyleBackColor = True
+        ' 
+        ' Button12
+        ' 
+        Button12.Font = New Font("Microsoft Sans Serif", 18F, FontStyle.Bold)
+        Button12.Location = New Point(248, 266)
+        Button12.Name = "Button12"
+        Button12.Size = New Size(65, 60)
+        Button12.TabIndex = 0
+        Button12.Text = "-"
+        Button12.UseVisualStyleBackColor = True
+        ' 
+        ' Button13
+        ' 
+        Button13.Font = New Font("Microsoft Sans Serif", 18F, FontStyle.Bold)
+        Button13.Location = New Point(177, 134)
+        Button13.Name = "Button13"
+        Button13.Size = New Size(65, 60)
+        Button13.TabIndex = 0
+        Button13.Text = "9"
+        Button13.UseVisualStyleBackColor = True
+        ' 
+        ' Button14
+        ' 
+        Button14.Font = New Font("Microsoft Sans Serif", 18F, FontStyle.Bold)
+        Button14.Location = New Point(248, 200)
+        Button14.Name = "Button14"
+        Button14.Size = New Size(65, 60)
+        Button14.TabIndex = 0
+        Button14.Text = "*"
+        Button14.UseVisualStyleBackColor = True
+        ' 
+        ' Button15
+        ' 
+        Button15.Font = New Font("Microsoft Sans Serif", 18F, FontStyle.Bold)
+        Button15.Location = New Point(319, 266)
+        Button15.Name = "Button15"
+        Button15.Size = New Size(65, 126)
+        Button15.TabIndex = 0
+        Button15.Text = "="
+        Button15.UseVisualStyleBackColor = True
+        ' 
+        ' Button16
+        ' 
+        Button16.Font = New Font("Microsoft Sans Serif", 18F, FontStyle.Bold)
+        Button16.Location = New Point(248, 134)
+        Button16.Name = "Button16"
+        Button16.Size = New Size(65, 60)
+        Button16.TabIndex = 0
+        Button16.Text = "/"
+        Button16.UseVisualStyleBackColor = True
+        ' 
+        ' Button17
+        ' 
+        Button17.Font = New Font("Microsoft Sans Serif", 18F, FontStyle.Bold)
+        Button17.Location = New Point(319, 134)
+        Button17.Name = "Button17"
+        Button17.Size = New Size(65, 60)
+        Button17.TabIndex = 0
+        Button17.Text = "⌫"
+        Button17.UseVisualStyleBackColor = True
+        ' 
+        ' Button18
+        ' 
+        Button18.Font = New Font("Microsoft Sans Serif", 18F, FontStyle.Bold)
+        Button18.Location = New Point(319, 200)
+        Button18.Name = "Button18"
+        Button18.Size = New Size(65, 60)
+        Button18.TabIndex = 0
+        Button18.Text = "C"
+        Button18.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.BackColor = SystemColors.Window
+        Label1.Font = New Font("Microsoft Sans Serif", 24F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label1.Location = New Point(35, 37)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(349, 60)
+        Label1.TabIndex = 2
+        Label1.TextAlign = ContentAlignment.MiddleRight
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(422, 433)
+        Controls.Add(Label1)
+        Controls.Add(Button15)
+        Controls.Add(Button18)
+        Controls.Add(Button17)
+        Controls.Add(Button16)
+        Controls.Add(Button14)
+        Controls.Add(Button13)
+        Controls.Add(Button12)
+        Controls.Add(Button11)
+        Controls.Add(Button10)
+        Controls.Add(Button9)
+        Controls.Add(Button8)
+        Controls.Add(Button7)
+        Controls.Add(Button6)
+        Controls.Add(Button5)
+        Controls.Add(Button4)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        FormBorderStyle = FormBorderStyle.FixedSingle
+        MaximizeBox = False
+        Name = "Form1"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "Calculator"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents Button11 As Button
+    Friend WithEvents Button12 As Button
+    Friend WithEvents Button13 As Button
+    Friend WithEvents Button14 As Button
+    Friend WithEvents Button15 As Button
+    Friend WithEvents Button16 As Button
+    Friend WithEvents Button17 As Button
+    Friend WithEvents Button18 As Button
+    Friend WithEvents Label1 As Label
+
+End Class
---
+++ Kalkulator Sederhana/Form1.vb
@@ -0,0 +1,87 @@
+﻿Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
+        If Label1.Text <> "0" Then Label1.Text &= "0"
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Label1.Text &= "1"
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Label1.Text &= "2"
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        Label1.Text &= "3"
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Label1.Text &= "4"
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        Label1.Text &= "5"
+    End Sub
+
+    Private Sub Button11_Click(sender As Object, e As EventArgs) Handles Button11.Click
+        Label1.Text &= "6"
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        Label1.Text &= "7"
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        Label1.Text &= "8"
+    End Sub
+
+    Private Sub Button13_Click(sender As Object, e As EventArgs) Handles Button13.Click
+        Label1.Text &= "9"
+    End Sub
+
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        Label1.Text &= "+"
+    End Sub
+
+    Private Sub Button12_Click(sender As Object, e As EventArgs) Handles Button12.Click
+        Label1.Text &= "-"
+    End Sub
+
+    Private Sub Button14_Click(sender As Object, e As EventArgs) Handles Button14.Click
+        Label1.Text &= "x"
+    End Sub
+
+    Private Sub Button16_Click(sender As Object, e As EventArgs) Handles Button16.Click
+        Label1.Text &= "/"
+    End Sub
+
+    Private Sub Button17_Click(sender As Object, e As EventArgs) Handles Button17.Click
+        If Label1.Text.Length > 0 Then
+            Label1.Text = Label1.Text.Remove(Label1.Text.Length - 1)
+        End If
+    End Sub
+
+    Private Sub Button18_Click(sender As Object, e As EventArgs) Handles Button18.Click
+        Label1.Text = Nothing
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Label1.Text &= "."
+    End Sub
+
+    Private Sub Button15_Click(sender As Object, e As EventArgs) Handles Button15.Click
+        Dim line As String
+        line = Label1.Text
+        Dim solv = New DataTable().Compute(line, Nothing)
+
+        If Label1.Text.Length > 0 Then
+            Label1.Text = solv
+        End If
+    End Sub
+End Class
